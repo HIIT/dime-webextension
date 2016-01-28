0564150f16084e3b437463aaa579896a57a97702
Mats Sjöberg <mats.sjoberg@helsinki.fi>
2016-01-28 13:51:00 +0200

    Refactored code, added skipping of given URLs

--- webextension/background.js
+++ webextension/background.js
@@ -1,59 +1,81 @@
-var settings = {
-    apiUrl: "http://localhost:8080/api",
-    username: "testuser",
-    password: "testuser123"
-}
+/*
+  Copyright (c) 2016 University of Helsinki
 
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation files
+  (the "Software"), to deal in the Software without restriction,
+  including without limitation the rights to use, copy, modify, merge,
+  publish, distribute, sublicense, and/or sell copies of the Software,
+  and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+// Ping DiMe
 var pingDiMe = function() {
     var req = new XMLHttpRequest();
     req.open("GET", apiUrl + "/ping");
     req.send(null);
 }
 
+// Log a visited web page to DiMe
 var logToDiMe = function(url, title, plainTextContent) {
-    console.log("Logging to DiMe: " + url + " - \"" + title + "\"");
+    var domain = domainFromUrl(url);
+    for (var skipUrl of skipSites) {
+        if (domain.indexOf(skipUrl) >= 0) {
+            console.log('DiMe logger: skipping ' + url)
+            return;
+        }
+    }
+
+    console.log('DiMe logger: logging ' + url);
     var data = JSON.stringify({
-        '@type': "DesktopEvent",
-        type: "http://www.semanticdesktop.org/ontologies/2010/01/25/nuao#UsageEvent",
-        actor: "DiMe browser extension",
+        '@type': 'DesktopEvent',
+        type: 'http://www.semanticdesktop.org/ontologies/2010/01/25/nuao#UsageEvent',
+        actor: 'DiMe browser extension',
         start: Date.now(),
         targettedResource: {
-            '@type': "Document",
-            isStoredAs: "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#RemoteDataObject",
-            mimeType: "text/html", 
+            '@type': 'Document',
+            isStoredAs: 'http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#RemoteDataObject',
+            mimeType: 'text/html', 
             plainTextContent: plainTextContent,
-            type: "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#Document", 
+            type: 'http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#Document', 
             uri: url, 
-            title: title, 
+            title: title,
             plainTextContent: plainTextContent
         }
     });
     var req = new XMLHttpRequest();
-    req.addEventListener("load", function() {
+    req.addEventListener('load', function() {
         if (this.status >= 400)
-            console.log("Error: " + this.statusText);
+            console.log('Error: ' + this.statusText);
     })
     
-    req.open("POST", settings.apiUrl + "/data/event");
-    req.setRequestHeader("Content-type", "application/json");
-    req.setRequestHeader("Authorization", "Basic " + 
-                         btoa(settings.username + ":" + settings.password));
+    req.open('POST', settings.apiUrl + '/data/event');
+    req.setRequestHeader('Content-type', 'application/json');
+    req.setRequestHeader('Authorization', 'Basic ' + 
+                         btoa(settings.username + ':' + settings.password));
     req.send(data);
 }
 
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-    if (namespace == "local") {
-        console.log("settings " + namespace);
-        for (key in changes) {
-            console.log("key " + key);
-            if (key in settings) {
-                console.log("changed key = " + changes[key].newValue);
-                settings[key] = changes[key].newValue;
-            }
-        }
-    }
-});
-
+// Handle message from page content
 chrome.runtime.onMessage.addListener(function(message) {
     logToDiMe(message.uri, message.title, message.text);
 })
+
+// Load settings on startup
+document.addEventListener('DOMContentLoaded', updateSettings);
+
+// Update settings when changed by the user
+chrome.storage.onChanged.addListener(settingsChangeListener);
--- webextension/options.js
+++ webextension/options.js
@@ -1,12 +1,42 @@
-var save_options = function() {
+/*
+  Copyright (c) 2016 University of Helsinki
+
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation files
+  (the "Software"), to deal in the Software without restriction,
+  including without limitation the rights to use, copy, modify, merge,
+  publish, distribute, sublicense, and/or sell copies of the Software,
+  and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+// Save values from HTML form to storage
+var saveOptions = function() {
   var apiUrl = document.getElementById('api_url').value;
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
+  var skipSites = document.getElementById('skip_sites').value;
+    
+  updateSkipSites(skipSites);
+  document.getElementById('skip_sites').value = settings.skipSites;
 
   chrome.storage.local.set({
       apiUrl: apiUrl,
       username: username,
-      password: password
+      password: password,
+      skipSites: skipSites
   }, function() {
     var status = document.getElementById('status');
       status.textContent = 'Options saved.';
@@ -16,18 +46,18 @@ var save_options = function() {
   });
 }
 
-var restore_options = function() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.local.get({
-      apiUrl: "http://localhost:8080/api",
-      username: "testuser",
-      password: "testuser123"
-  }, function(items) {
-    document.getElementById('api_url').value = items.apiUrl;
-    document.getElementById('username').value = items.username;
-    document.getElementById('password').value = items.password;
-  });
+var restoreOptions = function() {
+    updateSettings(function(items) {
+        // Update values in HTML form
+        document.getElementById('api_url').value = items.apiUrl;
+        document.getElementById('username').value = items.username;
+        document.getElementById('password').value = items.password;
+        document.getElementById('skip_sites').value = items.skipSites;
+    });
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+// Load settings on startup
+document.addEventListener('DOMContentLoaded', restoreOptions);
+
+// Save settings when user clicks "save"
+document.getElementById('save').addEventListener('click', saveOptions);
