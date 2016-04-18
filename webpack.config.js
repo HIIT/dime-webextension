65f0939a507489d7a317ae3ba71f6d0a13e6436b
Hung-Han Chen <chenhungh@gmail.com>
2016-04-18 10:26:36 +0300

    for webpack to read and build the extension

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+module.exports = {
+  entry: {
+    'dime.content': './src/chrome/dime.content.js',
+    'dime.logger': './src/chrome/dime.logger.js',
+    'dime.background': './src/chrome/dime.background.js',
+    'RankTermsWebWorker': './src/chrome/RankTermsWebWorker.js',
+  },
+  output: {
+    path: __dirname + '/build/chrome',
+    filename: '[name].js',
+  },
+  node: {
+    net: "empty",
+    tls: "empty",
+    fs: "empty",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: "json-loader"
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.md$/,
+        loader: "html!markdown"
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json']
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: './src/chrome/manifest.json',
+        to: 'manifest.json' //+output.path
+      },
+      {
+        from: './src/chrome/options.html',
+        to: 'options.html' //+output.path
+      },
+      {
+        from: 'src/chrome/icons/',
+        to: 'icons/' //+output.path
+      },
+    ], {
+      ignore: [],
+      copyUnmodified: true
+    })
+  ]
+};
