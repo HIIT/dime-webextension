25921042c69c041f92e29fbc5800435544727146
Hung-Han Chen <chenhungh@gmail.com>
2016-05-05 21:55:25 +0300

    minor code style refactor, remove "function" replaced with es6's "()=>", remove the vintage outer wrapper

--- src/chrome/dime.background.js
+++ src/chrome/dime.background.js
@@ -24,127 +24,122 @@
 
 import {settings} from './dime.common'
 
-;(function() {
-    var localEnableVar = settings.enable
+var localEnableVar = settings.enable
 
-    function setIcon () {
-        chrome.storage.local.get(['enable'], (items)=> {
-            let {enable} = items
-            if (typeof(enable) === "boolean")
-                if (enable)
-                    chrome.browserAction.setIcon(
-                        {path: {'19': 'icon19.png',
-                            '38': 'icon38.png'}});
-                else
-                    chrome.browserAction.setIcon(
-                        {path: {'19': 'icon19-disabled.png',
-                            '38': 'icon38-disabled.png'}});
-        })
-    }
-    function checkDiMeAlive () {
-        return new Promise((resolve, reject) => {
-            chrome.storage.local.get(['apiUrl'], (items)=> {
-                let {apiUrl} = items
-                if (apiUrl) {
-                    var req = new XMLHttpRequest()
-                    req.open("GET", apiUrl + "/ping", true)
-                    req.onreadystatechange = function() {
-                        if (req.status == 200) {
-                            resolve(req)
-                        } else {
-                            reject(req)
-                        }
-                    }
-                    req.send()
-                }
-
-            })
-        })
-
-    }
-
-    chrome.storage.local.get((items)=> {
-        let {apiUrl, username, password, skipSites, enable} = items
-        if (apiUrl) {} else { chrome.storage.local.set({apiUrl: settings.apiUrl}) }
-        if (username) {} else { chrome.storage.local.set({username: settings.username}) }
-        if (password) {} else { chrome.storage.local.set({password: settings.password}) }
-        if (skipSites) {} else { chrome.storage.local.set({skipSites: settings.skipSites}) }
-        if (typeof(enable) === "boolean") {
-            localEnableVar = enable
-        } else { chrome.storage.local.set({enable: settings.enable}, setIcon) }
+function setIcon () {
+    chrome.storage.local.get(['enable'], (items)=> {
+        let {enable} = items
+        if (typeof(enable) === "boolean")
+            if (enable)
+                chrome.browserAction.setIcon(
+                    {path: {'19': 'icon19.png',
+                        '38': 'icon38.png'}});
+            else
+                chrome.browserAction.setIcon(
+                    {path: {'19': 'icon19-disabled.png',
+                        '38': 'icon38-disabled.png'}});
     })
-
-
-    checkDiMeAlive().then((req)=> {
-        setIcon ()
-        chrome.storage.local.set({disconnected: false})
-    }).catch((req) => {
+}
+function checkDiMeAlive () {
+    return new Promise((resolve, reject) => {
         chrome.storage.local.get(['apiUrl'], (items)=> {
-            console.log(`connection with dime at ${items.apiUrl} error` )
-            console.log(req)
-            chrome.browserAction.setIcon(
-                {path: {'19': 'icon19-disconnected.png',
-                    '38': 'icon38-disconnected.png'}})
-        })
-        chrome.storage.local.set({disconnected: true})
-    })
-    // Browser action
-    chrome.browserAction.onClicked.addListener(function() {
-
-        localEnableVar = !localEnableVar
-        chrome.storage.local.set({enable: localEnableVar}, setIcon)
-
-    });
-
-    //detect URL changes
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-        if(changeInfo && changeInfo.status == "complete"){
-            chrome.tabs.sendMessage(tabId, {data: tab})
-        }
-    });
-
-
-
-    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        if (request.variable === 'enabled')
-            checkDiMeAlive().then((req)=> {
-                setIcon ()
-                chrome.storage.local.set({disconnected: false})
-            }).catch((req) => {
-                chrome.storage.local.get(['apiUrl'], (items)=> {
-                    console.log(`connection with dime at ${items.apiUrl} error` )
-                    console.log(req)
-                    chrome.browserAction.setIcon(
-                        {path: {'19': 'icon19-disconnected.png',
-                            '38': 'icon38-disconnected.png'}})
-                })
-                chrome.storage.local.set({disconnected: true})
-            })
-            sendResponse({
-                enabled: localEnableVar
-            });
-
-        if (request.dataWithDimeStructure) {
-            chrome.storage.local.get(['apiUrl', 'username', 'password'], (items)=> {
-                let {apiUrl, username, password} = items
-                var req = new XMLHttpRequest();
-                req.open('POST', apiUrl + '/data/event');
-                req.setRequestHeader('Content-type', 'application/json');
-                req.setRequestHeader('Authorization', 'Basic ' + btoa(username + ':' + password));
+            let {apiUrl} = items
+            if (apiUrl) {
+                var req = new XMLHttpRequest()
+                req.open("GET", apiUrl + "/ping", true)
                 req.onreadystatechange = function() {
-                    if (req.status == 200 && req.readyState == 4) {
-                        console.log(request.dataWithDimeStructure)
-                        console.log('sent to dime')
-                    }  else if ( req.readyState == 4 ) {
-                        console.log(request.dataWithDimeStructure)
-                        console.log('error occurs when sent to dime server')
-                        console.log(req)
+                    if (req.status == 200) {
+                        resolve(req)
+                    } else {
+                        reject(req)
                     }
                 }
-                req.send(JSON.stringify(request.dataWithDimeStructure));
+                req.send()
+            }
+
+        })
+    })
+
+}
+
+chrome.storage.local.get((items)=> {
+    let {apiUrl, username, password, skipSites, enable} = items
+    if (apiUrl) {} else { chrome.storage.local.set({apiUrl: settings.apiUrl}) }
+    if (username) {} else { chrome.storage.local.set({username: settings.username}) }
+    if (password) {} else { chrome.storage.local.set({password: settings.password}) }
+    if (skipSites) {} else { chrome.storage.local.set({skipSites: settings.skipSites}) }
+    if (typeof(enable) === "boolean") {
+        localEnableVar = enable
+    } else { chrome.storage.local.set({enable: settings.enable}, setIcon) }
+})
+
+
+checkDiMeAlive().then((req)=> {
+    setIcon ()
+    chrome.storage.local.set({disconnected: false})
+}).catch((req) => {
+    chrome.storage.local.get(['apiUrl'], (items)=> {
+        console.log(`connection with dime at ${items.apiUrl} error` )
+        console.log(req)
+        chrome.browserAction.setIcon(
+            {path: {'19': 'icon19-disconnected.png',
+                '38': 'icon38-disconnected.png'}})
+    })
+    chrome.storage.local.set({disconnected: true})
+})
+// Browser action
+chrome.browserAction.onClicked.addListener(() => {
+    localEnableVar = !localEnableVar
+    chrome.storage.local.set({enable: localEnableVar}, setIcon)
+
+});
+
+//detect URL changes
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if(changeInfo && changeInfo.status == "complete"){
+        chrome.tabs.sendMessage(tabId, {data: tab})
+    }
+});
+
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.variable === 'enabled')
+        checkDiMeAlive().then((req)=> {
+            setIcon ()
+            chrome.storage.local.set({disconnected: false})
+        }).catch((req) => {
+            chrome.storage.local.get(['apiUrl'], (items)=> {
+                console.log(`connection with dime at ${items.apiUrl} error` )
+                console.log(req)
+                chrome.browserAction.setIcon(
+                    {path: {'19': 'icon19-disconnected.png',
+                        '38': 'icon38-disconnected.png'}})
             })
-        }
-    });
+            chrome.storage.local.set({disconnected: true})
+        })
+        sendResponse({
+            enabled: localEnableVar
+        });
 
-
-}).call(this);
+    if (request.dataWithDimeStructure) {
+        chrome.storage.local.get(['apiUrl', 'username', 'password'], (items)=> {
+            let {apiUrl, username, password} = items
+            var req = new XMLHttpRequest();
+            req.open('POST', apiUrl + '/data/event');
+            req.setRequestHeader('Content-type', 'application/json');
+            req.setRequestHeader('Authorization', 'Basic ' + btoa(username + ':' + password));
+            req.onreadystatechange = function() {
+                if (req.status == 200 && req.readyState == 4) {
+                    console.log(request.dataWithDimeStructure)
+                    console.log('sent to dime')
+                }  else if ( req.readyState == 4 ) {
+                    console.log(request.dataWithDimeStructure)
+                    console.log('error occurs when sent to dime server')
+                    console.log(req)
+                }
+            }
+            req.send(JSON.stringify(request.dataWithDimeStructure));
+        })
+    }
+})
