aa1bb1686af12257888e9d7b47b74cb64806e2cb
Hung-Han Chen <chenhungh@gmail.com>
2016-05-05 23:35:10 +0300

    remove the injection of RankTermsWebworker.js

--- src/chrome/dime.content.js
+++ src/chrome/dime.content.js
@@ -5,23 +5,17 @@
         body = document.getElementsByTagName('body')[0],
         workerscript = document.createElement('script');
 
-    workerscript.src = chrome.runtime.getURL('RankTermsWebWorker.js');
-    workerscript.type = 'text/javascript';
-    workerscript.setAttribute('chrome', 'true');
-    workerscript.setAttribute('id', 'worker-script')
-    // Appending to body
-    body.appendChild(workerscript);
-
     script.src = chrome.runtime.getURL('dime.logger.js');
     script.type = 'text/javascript';
     script.setAttribute('chrome', 'true');
     // Appending to body
     body.appendChild(script);
 
-    window.addEventListener('message', function(event) {
+    window.addEventListener('message', (event) => {
       if(event.data && event.data.type && event.data.type === 'fromInjectedJStoHook') {
         console.log('hooking is done')
-        chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+          console.log(`tabs.status = ${request.data.status}`)
           if (request.data.url) {
               console.log('url changed, parsing page')
               window.postMessage({
