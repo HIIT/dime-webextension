efa747f1d9b343e0c36f7d2403243df9f71db97d
Hung-Han Chen <chenhungh@gmail.com>
2016-06-23 14:13:10 +0300

    move most of computation intensive stuffs to dime.background script and add checking for recording duplicated URLs

--- src/chrome/dime.background.js
+++ src/chrome/dime.background.js
@@ -1,28 +1,18 @@
-/*
- Copyright (c) 2016 University of Helsinki
+import domurl from 'domurl'
+import $ from 'jquery'
+import ineed from 'ineed'
+import _ from 'underscore'
+import readability from 'readability-js'
+import cheerio from 'cheerio'
+import getTextTokens from './getTextTokens'
 
- Permission is hereby granted, free of charge, to any person
- obtaining a copy of this software and associated documentation files
- (the "Software"), to deal in the Software without restriction,
- including without limitation the rights to use, copy, modify, merge,
- publish, distribute, sublicense, and/or sell copies of the Software,
- and to permit persons to whom the Software is furnished to do so,
- subject to the following conditions:
-
- The above copyright notice and this permission notice shall be
- included in all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
- */
-
-import {settings} from './dime.common'
+const defaultSettings = {
+    apiUrl: 'http://localhost:8080/api',
+    username: 'testuser',
+    password: 'testuser123',
+    skipSites: ['localhost:8080'],
+    enable: true
+}
 
 function setIconByConnectionStatus (disconneted) {
     if (disconneted) {
@@ -60,10 +50,10 @@ chrome.browserAction.onClicked.addListener(() => {
 
 chrome.storage.sync.get((v)=> {
     let {apiUrl, username, password, skipSites, enable} = v
-    apiUrl = apiUrl || chrome.storage.sync.set({'apiUrl': settings.apiUrl})
-    username = username ||  chrome.storage.sync.set({'username': settings.username})
-    password = password || chrome.storage.sync.set({'password': settings.password})
-    skipSites = skipSites ||  chrome.storage.sync.set({'skipSites': settings.skipSites})
+    apiUrl = apiUrl || chrome.storage.sync.set({'apiUrl': defaultSettings.apiUrl})
+    username = username ||  chrome.storage.sync.set({'username': defaultSettings.username})
+    password = password || chrome.storage.sync.set({'password': defaultSettings.password})
+    skipSites = skipSites ||  chrome.storage.sync.set({'skipSites': defaultSettings.skipSites})
     if (typeof enable === 'undefined') chrome.storage.sync.set({'enable': true})
 })
 
@@ -146,27 +136,66 @@ function checkEnable () {
     })
 }
 
+function checkBlockList (tab) {
+    return new Promise((resolve, reject) => {
+        chrome.storage.sync.get(['skipSites'], (v)=> {
+            let skipSiteArray = []
+            let currentURL = new domurl(tab.url)
+            let hostPlusPort = currentURL.host + ((currentURL.port.length > 0)? (':'+ currentURL.port) : '')
+
+            for (var line of v.skipSites.split('\n')) {
+                if (line !== '') {
+                    skipSiteArray.push(line);
+                }
+            }
+            let WWWskipSiteArray = skipSiteArray.map((i)=> {return 'www.' + i})
+            if (skipSiteArray.indexOf(hostPlusPort) === -1 && WWWskipSiteArray.indexOf(hostPlusPort) === -1) {
+                resolve(true)
+            } else {
+                console.log('url in blocklist')
+                resolve(false)
+            }
+        })
+    })
+}
 async function init (tabId, tab) {
     let alive = await checkDiMeAlive()
     let enable = await checkEnable()
-    if (alive && enable) {
+    let notInBlockList = await checkBlockList (tab)
+    //console.log("notInBlockList = " + notInBlockList)
+
+    if (alive && enable && notInBlockList) {
+        //console.log(tab)
         unblockEvalinScrtipSrc()
         chrome.tabs.sendMessage(tabId, {data: tab})
+        console.log('tab data sent to content script')
     }
 }
 
 checkDiMeAlive ()
 //detect URL changes
+var visitedURL = null
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if(changeInfo.status && changeInfo.status === 'complete') {
-        init(tabId, tab)
+    if(changeInfo.status && changeInfo.status === 'complete' && tab.url) {
+        //console.log(changeInfo)
+        //console.log(tabId)
+        //console.log(tab.url)
+        if (tab.url === visitedURL) {
+            console.log('a duplicated url: ' +  visitedURL)
+
+        } else {
+            console.log(visitedURL)
+            console.log(tab.url)
+            visitedURL = tab.url
+            //console.log('call init')
+            init(tabId, tab)
+        }
+
     }
 })
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.dataWithDimeStructure) {
-        //console.log(request.dataWithDimeStructure)
-        chrome.storage.sync.get(['apiUrl', 'username', 'password'], (items)=> {
+function sendToDiMe (dataWithDimeStructure) {
+    chrome.storage.sync.get(['apiUrl', 'username', 'password'], (items)=> {
             let {apiUrl, username, password} = items
             var req = new XMLHttpRequest()
             req.open('POST', apiUrl + '/data/event')
@@ -174,17 +203,178 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             req.setRequestHeader('Authorization', 'Basic ' + btoa(username + ':' + password))
             req.onreadystatechange = function() {
                 if (req.status == 200 && req.readyState == 4) {
-                    console.log(request.dataWithDimeStructure)
+                    console.log(dataWithDimeStructure)
                     console.log('sent to dime')
                 }  else if ( req.readyState == 4 ) {
                     //console.log(request.dataWithDimeStructure)
-                    console.log(request.dataWithDimeStructure)
+                    console.log(dataWithDimeStructure)
                     console.log('error occurs when sent to dime server')
                     console.log(req)
                 }
             }
+            req.send(JSON.stringify(dataWithDimeStructure))
+        })
 
-            req.send(JSON.stringify(request.dataWithDimeStructure))
+}
+function getFrequentWords(content, numberOfWords) {
+    return new Promise((resolve) => {
+        let tokensWithOutStopWords = getTextTokens(content)
+        let frequencies = {}
+        for (let word of tokensWithOutStopWords) {
+            frequencies[word] = frequencies[word] || 0;
+            frequencies[word]++;
+        }
+        let words = Object.keys( frequencies );
+        let wordsRankedByFrequent = words.sort((a,b) => {
+            return frequencies[b] -frequencies[a];
+        }).slice(0,numberOfWords)
+        resolve(wordsRankedByFrequent)
+    })
+}
+function getOpenGraphProtocol() {
+    return new Promise((resolve, reject) => {
+        resolve(_.pick({
+            title: $('meta[property="og:title"]').attr('content'),
+            type: $('meta[property="og:type"]').attr('content'),
+            image: $('meta[property="og:image"]').attr('content'),
+            //url: $('meta[property="og:url"]').attr('content'),
+            site_name: $('meta[property="og:site_name"]').attr('content'),
+            audio: $('meta[property="og:audio"]').attr('content'),
+            video: $('meta[property="og:video"]').attr('content'),
+            description: $('meta[property="og:description"]').attr('content'),
+            determiner: $('meta[property="og:determiner"]').attr('content'),
+            locale: $('meta[property="og:locale"]').attr('content'),
+        }, _.identity))
+    })
+}
+function getMetaTags() {
+    return new Promise((resolve, reject) => {
+        resolve($("head meta").map(function (index,meta) {
+            if (meta.name && meta.content)
+                return {
+                    name: meta.name,
+                    content: meta.content
+                }
+        }).toArray())
+    })
+}
+function getArticle(document) {
+    return new Promise((resolve, reject) => {
+        readability(document.innerHTML, (err, article, meta) => {
+            resolve(article);
+        });
+    })
+}
+async function compile(document, url) {
+    let pageData = {}
+    let article = await getArticle(document)
+    if (article && article.content) {
+        let content = article.content.text()
+        let frequentTerms = await getFrequentWords(content, 10)
+        let articleHTML = cheerio.load(article.content.html())('*').removeAttr('class').removeAttr('id').removeAttr('style').html()
+        let innedCollection = ineed.collect.images.hyperlinks.fromHtml(articleHTML)
+        pageData.tags = frequentTerms.slice(0,8).map((term)=> {
+            return {
+                '@type': 'Tag',
+                text: term,
+                //auto: true,
+                //date: Date.now()
+            }
+        })
+        pageData.frequentTerms = frequentTerms
+        if (article.excerpt) {pageData.abstract = article.excerpt}
+        pageData.HTML = articleHTML
+        pageData.plainTextContent = content
+        pageData.imgURLs = innedCollection.images.map((i)=> { return {url: 'http:' + i.src, text: i.alt }})
+        pageData.hyperLinks =  innedCollection.hyperlinks.map((i)=> { return {url: (i.href.indexOf('http') === -1)? url + i.href: i.href, text: i.text}})
+        pageData.pageURL = url
+        //pageData.openGraphProtocol = await getOpenGraphProtocol()
+        //pageData.metaTags = await getMetaTags()
+        pageData.title = document.title
+        pageData.document = document
+    }
+    return pageData
+
+}
+var compiledURL = null
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.document && request.url !== compiledURL) {
+        let {document, url} = request
+        compiledURL = url
+        //console.log(request)
+        //console.log(document)
+        compile(document, url).then((pageData)=> {
+            if (_.isEmpty(pageData) || pageData.plainTextContent.length < 100) {
+                //console.log('no valuable text content found, send title/url/plain HTML only')
+                const simplePageDataWithDimeStructure = {
+                    '@type': 'DesktopEvent',
+                    type: 'http://www.semanticdesktop.org/ontologies/2010/01/25/nuao/#UsageEvent',
+                    actor: 'DiMe browser extension',
+                    start: Date.now(),
+                    targettedResource: {
+                        '@type': 'WebDocument',
+                        title: document.title,
+                        isStoredAs: 'http://www.semanticdesktop.org/ontologies/2007/03/22/nfo/#RemoteDataObject',
+                        type: 'http://www.semanticdesktop.org/ontologies/2007/03/22/nfo/#HtmlDocument',
+                        mimeType: 'text/html',
+                        plainTextContent: document.innerText,
+                        uri: url,
+                    }
+                }
+                sendToDiMe(simplePageDataWithDimeStructure)
+                console.log('simple page data sent to dime')
+                return
+            }
+            const pageDataWithDimeStructure = {
+                '@type': 'DesktopEvent',
+                type: 'http://www.semanticdesktop.org/ontologies/2010/01/25/nuao/#UsageEvent',
+                actor: 'DiMe browser extension',
+                start: Date.now(),
+                targettedResource: {
+                    '@type': 'WebDocument',
+                    title: pageData.document.title,
+                    isStoredAs: 'http://www.semanticdesktop.org/ontologies/2007/03/22/nfo/#RemoteDataObject',
+                    type: 'http://www.semanticdesktop.org/ontologies/2007/03/22/nfo/#HtmlDocument',
+                    mimeType: 'text/html',
+                    //tags: pageData.tags,
+                    plainTextContent: pageData.plainTextContent,
+                    uri: pageData.pageURL,
+                    frequentTerms: pageData.frequentTerms,
+                    HTML: pageData.HTML,
+                    abstract: pageData.abstract,
+                    imgURLs: pageData.imgURLs,
+                    hyperLinks: pageData.hyperLinks,
+                    //OpenGraphProtocol: pageData.openGraphProtocol,
+                    //MetaTags: pageData.MetaTags,
+                }
+            }
+            sendToDiMe(pageDataWithDimeStructure)
         })
     }
+    else {
+        console.log('url has been compiled, skip.')
+    }
+    //if (request.dataWithDimeStructure) {
+    //    //console.log(request.dataWithDimeStructure)
+    //    chrome.storage.sync.get(['apiUrl', 'username', 'password'], (items)=> {
+    //        let {apiUrl, username, password} = items
+    //        var req = new XMLHttpRequest()
+    //        req.open('POST', apiUrl + '/data/event')
+    //        req.setRequestHeader('Content-type', 'application/json')
+    //        req.setRequestHeader('Authorization', 'Basic ' + btoa(username + ':' + password))
+    //        req.onreadystatechange = function() {
+    //            if (req.status == 200 && req.readyState == 4) {
+    //                console.log(request.dataWithDimeStructure)
+    //                console.log('sent to dime')
+    //            }  else if ( req.readyState == 4 ) {
+    //                //console.log(request.dataWithDimeStructure)
+    //                console.log(request.dataWithDimeStructure)
+    //                console.log('error occurs when sent to dime server')
+    //                console.log(req)
+    //            }
+    //        }
+    //
+    //        req.send(JSON.stringify(request.dataWithDimeStructure))
+    //    })
+    //}
 })
--- src/chrome/dime.content.js
+++ src/chrome/dime.content.js
@@ -1,3 +1,16 @@
+//function injectScript() {
+//  let script = document.createElement('script')
+//  let body = document.getElementsByTagName('body')[0]
+//  script.src = chrome.runtime.getURL('dime.logger.js')
+//  script.type = 'text/javascript';
+//  script.setAttribute('chrome', 'true')
+//  body.appendChild(script);
+//  window.addEventListener('message', function(event) {
+//    if(event.data && event.data.type && event.data.type === 'pageDataWithDimeStructure') {
+//      chrome.runtime.sendMessage({dataWithDimeStructure: event.data.payload})
+//    }
+//  })
+//}
 function injectScript() {
   let script = document.createElement('script')
   let body = document.getElementsByTagName('body')[0]
@@ -6,21 +19,24 @@ function injectScript() {
   script.setAttribute('chrome', 'true')
   body.appendChild(script);
   window.addEventListener('message', function(event) {
-    if(event.data && event.data.type && event.data.type === 'pageDataWithDimeStructure') {
-      chrome.runtime.sendMessage({dataWithDimeStructure: event.data.payload})
+    if(event.data && event.data.type && event.data.type === 'document') {
+      chrome.runtime.sendMessage({document:  event.data.payload, url: window.location.href})
+      console.log('send document to background script')
     }
   })
 }
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  //console.log(request)
   if (request.data.status === 'complete') {
-    console.log('tab url changed')
-    chrome.storage.sync.get(['skipSites'], (v)=> {
-      let {skipSites} = v
-      const notLocalhost = /^http:\/\/\w+(\.\w+)*(:[0-9]+)?\/?(\/[.\w]*)*$/
-      if (skipSites.indexOf(window.location.href.replace(/.*?:\/\//g, "")) === -1 && !notLocalhost.test(window.location.href))
-        //console.log(`site is not in the filter list: ${skipSites}, continue`)
-        injectScript();
-    })
+    injectScript()
+    //console.log('tab url changed')
+    //chrome.storage.sync.get(['skipSites'], (v)=> {
+    //  let {skipSites} = v
+    //  const notLocalhost = /^http:\/\/\w+(\.\w+)*(:[0-9]+)?\/?(\/[.\w]*)*$/
+    //  if (skipSites.indexOf(window.location.href.replace(/.*?:\/\//g, "")) === -1 && !notLocalhost.test(window.location.href))
+    //    //console.log(`site is not in the filter list: ${skipSites}, continue`)
+    //    injectScript();
+    //})
   }
 })
 
