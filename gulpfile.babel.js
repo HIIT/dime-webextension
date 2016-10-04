09e9cc030636a755628838d6b8cba0c10aea927b
Hung-Han Chen <chenhungh@gmail.com>
2016-10-04 22:41:21 +0300

    and two new gulp script for Chrome extension

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,21 +1,28 @@
-'use strict';
-
-import gulp from 'gulp'
-import crx from 'gulp-crx-pack'
-import rseq  from 'run-sequence'
-//import clean from 'gulp-clean'
-import manifest from './src/chrome/manifest.json'
+import gulp from 'gulp';
+import zip from 'gulp-zip';
+import rseq from 'run-sequence';
+import crx from 'gulp-crx-pack';
 import fs from 'fs';
 
-gulp.task('chrome', function () {
-    gulp.src('./build/chrome')
-        .pipe(crx({
-            privateKey: fs.readFileSync('./src/certs/chrome.pem', 'utf8'),
-            filename: 'dime-chrome-extension-v' + manifest.version + '.crx'
-        }))
-        .pipe(gulp.dest('./dist/chrome'));
+gulp.task('chrome-crx', () => {
+  gulp.src('./build/webextension/**/*')
+    .pipe(crx({
+      privateKey: fs.readFileSync('./src/certs/key.pem', 'utf8'),
+      filename: 'dime-chrome-extension.crx',
+    }))
+    .pipe(gulp.dest('./dist/chrome'));
 });
 
-gulp.task('default', function() {
-    return rseq(['chrome']);
+gulp.task('chrome-zip', () => {
+  gulp.src(['./dist/chrome/dime-chrome-extension.crx', './src/certs/key.pem'])
+    .pipe(zip('dime-chrome-extension.zip'))
+    .pipe(gulp.dest('./dist/chrome'));
 });
+
+
+gulp.task('safari', () => {
+  gulp.src('./build/safari/**/*')
+    .pipe(gulp.dest('./dist/safari/dime-safari-extension.safariextension'));
+});
+
+gulp.task('default', () => rseq(['chrome']));
